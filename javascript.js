941e3e2845342eb3d80c6fb72a63652ba7d13fce
Accellerant <accellerant@gmail.com>
2022-07-01 19:49:59 -0700

    Baseline files

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const htmlBody = document.querySelector("body");
+const extraH1 = document.createElement('h1');
+extraH1.textContent = 'This is a test to see if the JS was linked properly';
+
+htmlBody.appendChild(extraH1);
