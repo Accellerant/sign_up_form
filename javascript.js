524a8f25078206558c2ad6094949d1eec6ae99a3
Accellerant <accellerant@gmail.com>
2022-07-05 20:52:59 -0700

    Cleared since test No Longer Needed

--- javascript.js
+++
@@ -1,5 +0,0 @@
-const htmlBody = document.querySelector("body");
-const extraH1 = document.createElement('h1');
-extraH1.textContent = 'This is a test to see if the JS was linked properly';
-
-htmlBody.appendChild(extraH1);
